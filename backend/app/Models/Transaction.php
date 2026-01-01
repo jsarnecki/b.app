4b2c01a3ef441d6b42513df845994c6ed13c5c5e
Josh Sarnecki <joshsarnecki@gmail.com>
2026-01-01 10:56:03 -0700

    Delete expense model

--- backend/app/Models/Transaction.php
+++ backend/app/Models/Transaction.php
@@ -5,8 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Expense extends Model
+class Transaction extends Model
 {
-    /** @use HasFactory<\Database\Factories\ExpenseFactory> */
+    /** @use HasFactory<\Database\Factories\TransactionFactory> */
     use HasFactory;
 }
