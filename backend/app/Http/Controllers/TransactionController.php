ebe9160d311d373f5537b0942e4628cef9aa8ea1
Josh Sarnecki <joshsarnecki@gmail.com>
2026-01-24 15:13:33 -0800

    Begins building transaction controller, sets up fillable on model

--- backend/app/Http/Controllers/TransactionController.php
+++ backend/app/Http/Controllers/TransactionController.php
@@ -2,9 +2,60 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Transaction;
 use Illuminate\Http\Request;
 
 class TransactionController extends Controller
 {
-    //
+    /**
+     *
+     */
+    public function index(): array
+    {
+        return [];
+    }
+
+
+    /**
+     *
+     */
+    public function show(): array
+    {
+        return [];
+    }
+
+
+    /**
+     *
+     */
+    public function store(Request $request): array
+    {
+        $data = $request->all();
+        $transaction = Transaction::create([
+            'user_id' => $data['user_id'],
+            'type' => $data['type'],
+            'category' => $data['category'],
+            'amount' => $data['amount'],
+            'description' => $data['description'],
+            'transaction_date' => $data['transaction_date'],
+        ]);
+
+        return ['id' => $transaction->id];
+    }
+
+    /**
+     *
+     */
+    public function update(): array
+    {
+        return [];
+    }
+
+    /**
+     *
+     */
+    public function destroy(): array
+    {
+        return [];
+    }
 }
--- backend/app/Models/Transaction.php
+++ backend/app/Models/Transaction.php
@@ -9,4 +9,18 @@ class Transaction extends Model
 {
     /** @use HasFactory<\Database\Factories\TransactionFactory> */
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'user_id',
+        'type',
+        'category',
+        'amount',
+        'description',
+        'transaction_date',
+    ];
 }
