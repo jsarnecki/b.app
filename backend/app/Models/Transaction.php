f7eab1d18497fabf0d286aa651a2c87ff3bfc9b6
Josh Sarnecki <joshsarnecki@gmail.com>
2026-01-27 21:57:55 -0800

    Adds relationships to user and transaction models

--- backend/app/Models/Transaction.php
+++ backend/app/Models/Transaction.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Transaction extends Model
 {
@@ -23,4 +24,16 @@ class Transaction extends Model
         'description',
         'transaction_date',
     ];
+
+    // region Relationships
+
+    /**
+     * Get the user that owns the transaction.
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    // endregion
 }
