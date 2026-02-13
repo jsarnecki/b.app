5f3a811c5e1b4f0c8b1d9e742846ce6ec31c172b
Josh Sarnecki <joshsarnecki@gmail.com>
2026-02-13 14:25:28 -0800

    Sets up create_transaction api request

--- mobile/src/components/TransactionForm.tsx
+++ mobile/src/components/TransactionForm.tsx
@@ -1,7 +1,10 @@
 import React, { useState } from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, Keyboard } from 'react-native';
 import { TextInput, Button } from 'react-native-paper';
 import MoneyInput from './MoneyInput';
+import Constants from 'expo-constants';
+
+const API_URL = Constants.expoConfig?.extra?.apiUrl;
 
 const TransactionForm: React.FC = () => {
   const [type, setType] = useState('expense');
@@ -11,25 +14,49 @@ const TransactionForm: React.FC = () => {
   const [date, setDate] = useState(new Date().toISOString().split('T')[0]);
 
   const handleSubmit = async () => {
-    const currentDate = new Intl.DateTimeFormat('en', {
-      dateStyle: 'short',
-      timeStyle: 'short',
-      hour12: false
-    }).format(new Date());
+    Keyboard.dismiss();
 
-    // needs validation
-    const transaction = {
-      user_id: 1, // Hardcode for now
-      type,
-      category,
-      amount: parseFloat(amount),
-      description,
-      transaction_date: date,
-      justDate: currentDate
-    };
+    try {
 
-    console.log('Submitting:', transaction);
-    // api call...
+      const currentDate = new Intl.DateTimeFormat('en', {
+        dateStyle: 'short',
+        timeStyle: 'short',
+        hour12: false
+      }).format(new Date());
+
+      // needs validation
+      const transaction = {
+        user_id: 1, // Hardcode for now
+        type,
+        category,
+        amount: parseFloat(amount),
+        description,
+        transaction_date: date,
+        justDate: currentDate
+      };
+
+      const response = await fetch(`${API_URL}/create_transaction`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(transaction),
+      });
+
+      console.log('Submitting:', transaction);
+      const data = await response.json();
+
+      if (response.ok) {
+        setCategory('');
+        setAmount('');
+        setDescription('');
+
+      } else {
+        console.error('Response not ok.');
+      }
+    } catch (error) {
+      console.error('Caught error:' + error);
+    }
   };
 
   return (
