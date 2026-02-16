e5b5df168b58d9ebe65e10d93a9a6b509bcf3960
Josh Sarnecki <joshsarnecki@gmail.com>
2026-02-15 16:03:55 -0800

    Cleans up validation, adds error handling

--- backend/app/Http/Controllers/TransactionController.php
+++ backend/app/Http/Controllers/TransactionController.php
@@ -4,8 +4,11 @@ namespace App\Http\Controllers;
 
 use App\Models\Transaction;
 use App\Models\User;
+use Exception;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Validation\ValidationException;
 
 class TransactionController extends Controller
 {
@@ -14,8 +17,10 @@ class TransactionController extends Controller
      */
     public function index(Request $request): JsonResponse
     {
-        $transactions = User::findOrFail($request->all()['id'])->transactions;
-        return response()->json($transactions);
+        // Handle auth verification.
+        $user = User::findOrFail($request->all()['id']);
+        /* $transactions = $user->transactions; */
+        return response()->json($user->transactions);
     }
 
 
@@ -30,28 +35,25 @@ class TransactionController extends Controller
     /**
      *
      */
-    public function store(Request $request): array
+    public function store(Request $request): JsonResponse
     {
-        /* $data = $request->all(); */
         $data = $request->validate([
             'user_id' => 'required|integer', // eventually handle this by tying to auth user instead of as a param
             'type' => 'required|string|max:255',
             'category' => 'required|string|max:255',
-            'amount'  => 'required|decimal:2',
-            'description'  => 'nullable|string',
-            'transaction_date'  => 'required|date', // eventually add specific format [Rule::date()->format(ideal timestamp)]
+            'amount' => 'required|numeric|min:0',
+            'description' => 'nullable|string',
+            'transaction_date' => 'required|date', // eventually add specific format [Rule::date()->format(ideal timestamp)]
         ]);
 
-        $transaction = Transaction::create([
-            'user_id' => $data['user_id'],
-            'type' => $data['type'],
-            'category' => $data['category'],
-            'amount' => $data['amount'],
-            'description' => $data['description'],
-            'transaction_date' => $data['transaction_date'],
-        ]);
-
-        return ['id' => $transaction->id];
+        try {
+            $transaction = Transaction::create($data);
+        } catch (Exception $error) {
+            Log::error('Failed to create transaction: ' . $error->getMessage());
+            /* throw new Exception('Store error: ' . $error); */
+            return response()->json(['error' => 'Failed to save transaction'], 500);
+        }
+        return response()->json(['id' => $transaction->id]);
     }
 
     /**
