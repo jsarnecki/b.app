2f8fc08662aaf3116ea245e7533ad00fa2ac648e
Josh Sarnecki <joshsarnecki@gmail.com>
2026-02-25 21:47:39 -0800

    Sets up provider for snackbar

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -2,12 +2,15 @@ import { PaperProvider } from 'react-native-paper';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 // import HomeScreen from './src/screens/HomeScreen';
 import AppNavigator from './src/navigation/AppNavigator';
+import SnackbarProvider from './src/providers/SnackbarProvider';
 
 export default function App() {
   return (
     <SafeAreaProvider>
       <PaperProvider >
-        <AppNavigator />
+        <SnackbarProvider>
+          <AppNavigator />
+        </SnackbarProvider>
       </PaperProvider>
     </SafeAreaProvider>
   );
---
+++ mobile/src/providers/SnackbarProvider.tsx
@@ -0,0 +1,46 @@
+import { createContext, useContext, useState } from "react";
+import { Portal, Snackbar } from "react-native-paper";
+
+interface SnackbarContextType {
+  showSnackbar: (message: string) => void;
+}
+
+const SnackbarContext = createContext<SnackbarContextType | undefined>(undefined);
+
+export default function SnackbarProvider({ children }: { children: React.ReactNode }) {
+  const [visible, setVisible] = useState(false);
+  const [message, setMessage] = useState('');
+
+  const showSnackbar = (msg: string) => {
+    setMessage(msg);
+    setVisible(true);
+  };
+
+  return (
+    <Portal>
+      <SnackbarContext.Provider value={{ showSnackbar }}>
+        {children}
+        <Snackbar
+          visible={visible}
+          onDismiss={() => setVisible(false)}
+          duration={3000}
+          action={{
+            label: 'Dismiss',
+            onPress: () => setVisible(false),
+          }}
+        // wrapperStyle={{ top: 0, bottom: 'auto' }}
+        >
+          {message}
+        </Snackbar>
+      </SnackbarContext.Provider>
+    </Portal>
+  )
+}
+
+export function useSnackbar() {
+  const context = useContext(SnackbarContext);
+  if (!context) {
+    throw new Error('useSnackbar must be used within a SnackbarProvider');
+  }
+  return context;
+}
