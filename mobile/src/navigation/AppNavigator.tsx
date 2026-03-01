b23ff2d57c18bc80b88e3a3660981e0e8514a222
Josh Sarnecki <joshsarnecki@gmail.com>
2026-03-01 14:08:55 -0800

    Adds icons to bottom tabs

--- mobile/src/navigation/AppNavigator.tsx
+++ mobile/src/navigation/AppNavigator.tsx
@@ -3,17 +3,36 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import HomeScreen from "../screens/HomeScreen";
 import TransactionListScreen from "../screens/TransactionListScreen";
 import SettingsScreen from "../screens/SettingsScreen";
+import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
+
+type IconName = React.ComponentProps<typeof MaterialCommunityIcons>["name"];
 
 const Tab = createBottomTabNavigator();
 
 export default function AppNavigator() {
 
+  const renderOptions = (iconName: IconName) => ({
+    tabBarIcon: () => <MaterialCommunityIcons name={iconName} color="black" size={21} />
+  });
+
   return (
     <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="Settings" component={SettingsScreen} />
-        <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen name="List" component={TransactionListScreen} />
+      <Tab.Navigator initialRouteName="Home">
+        <Tab.Screen
+          name="Settings"
+          component={SettingsScreen}
+          options={renderOptions('cog')}
+        />
+        <Tab.Screen
+          name="Home"
+          component={HomeScreen}
+          options={renderOptions('home')}
+        />
+        <Tab.Screen
+          name="List"
+          component={TransactionListScreen}
+          options={renderOptions('format-list-bulleted-triangle')}
+        />
       </Tab.Navigator>
     </NavigationContainer>
   )
