d1578f9810f83a0791420004396daab5938a4ef3
Josh Sarnecki <joshsarnecki@gmail.com>
2026-03-20 20:54:52 -0700

    Adds a deleteJson helper

--- mobile/src/api/api.ts
+++ mobile/src/api/api.ts
@@ -3,6 +3,11 @@ import Constants from "expo-constants";
 
 const API_URL = Constants.expoConfig?.extra?.apiUrl;
 
+interface Response {
+  message?: string;
+  error?: Record<string, string> | string;
+}
+
 const getHeaders = async () => {
   const token = await AsyncStorage.getItem('auth_token')
   return {
@@ -10,6 +15,16 @@ const getHeaders = async () => {
     'Accept': 'application/json',
     'Authorization': `Bearer ${token}`
   }
+}
+
+const handleErrors = (data: Response) => {
+  let error = data.message || 'An unexpected error occurred.';
+
+  if (data.error) {
+    // Chain all error messages together. 
+    error = Object.values(data.error).flat().join(', ');
+  }
+  throw new Error(error);
 }
 
 export const getJson = async (route: string) => {
@@ -20,13 +35,7 @@ export const getJson = async (route: string) => {
 
   const data = await response.json();
   if (!response.ok) {
-    let error = data.message || 'An unexpected error occurred.';
-
-    if (data.error) {
-      // Chain all error messages together. 
-      error = Object.values(data.error).flat().join(', ');
-    }
-    throw new Error(error);
+    handleErrors(data);
   }
   return data;
 }
@@ -40,14 +49,20 @@ export const postJson = async (route: string, body: Record<string, unknown>) => 
 
   const data = await response.json();
   if (!response.ok) {
-    let error = data.message || 'An unexpected error occurred.';
-
-    if (data.error) {
-      // Chain all error messages together. 
-      error = Object.values(data.error).flat().join(', ');
-    }
-    throw new Error(error);
+    handleErrors(data);
   }
   return data;
 }
 
+export const deleteJson = async (route: string) => {
+  const response = await fetch(`${API_URL}/${route}`, {
+    method: 'DELETE',
+    headers: await getHeaders()
+  });
+
+  const data = await response.json();
+  if (!response.ok) {
+    handleErrors(data);
+  }
+  return data;
+};
