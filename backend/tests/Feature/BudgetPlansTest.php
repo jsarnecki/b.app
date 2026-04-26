727ca3cf75cafd501db9b90e6c1d4d87ad95f0ba
Josh Sarnecki <joshsarnecki@gmail.com>
2026-04-25 20:04:43 -0700

    Begins writing out feature tests

--- backend/tests/Feature/BudgetPlansTest.php
+++ backend/tests/Feature/BudgetPlansTest.php
@@ -3,13 +3,91 @@
 namespace Tests\Feature;
 
 // use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Models\BudgetPlan;
+use App\Models\BudgetPlanEnvelope;
+use App\Services\BudgetPeriodService;
+use Carbon\Carbon;
+use TClass;
 use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class BudgetPlansTest extends TestCase
 {
+    use RefreshDatabase;
+
+    private $budgetPeriodService;
+
     public function setUp(): void
     {
         parent::setUp();
+        $this->budgetPeriodService = app(BudgetPeriodService::class);
+    }
+
+    /**
+     * Tests with null previous end date.
+     */
+    public function testGeneratePeriodNoPreviousEndDate(): void
+    {
+        /* $this->assertTrue(true); */
+
+        /* $plan = BudgetPlan::factory([ */
+        /*     'user_id'      => 1, */
+        /*     'name'         => 'test', */
+        /*     'period_type'  => 'monthly', */
+        /*     'total_amount' => 500, */
+        /*     'starts_at'    => Carbon::now()->format('y-m-d'), */
+        /*     'ends_at'      => null, */
+        /* ]); */
+
+        $plan = BudgetPlan::factory()
+            ->for(BudgetPlanEnvelope::factory()
+                ->count(3)
+                ->create())
+            ->create([
+                'starts_at'    => Carbon::now()->format('Y-m-d'),
+                'ends_at'      => null,
+            ]);
+
+        // create PlanEnvelopes for BudgetPlan->associate()
+        /* $envelopes = BudgetPlanEnvelope::factory() */
+        /*     ->count(3) */
+        /*     ->for($plan) */
+        /*     ->create(); */
+
+        /* $envelopes->each(function ($envelope) use ($plan) { */
+        /*     $plan->planEnvelope->associate($envelope); */
+        /* }); */
+
+        $period = $this->budgetPeriodService->generatePeriod($plan, null);
+        // assert period has plan_id, ..
+        $this->assertSame($plan->id, $period->budget_plan_id);
+        // period start date is same as plan (when null passed) and end date is end of the month
+        $this->assertSame(Carbon::parse($plan->starts_at)->startOfDay(), Carbon::parse($period->start_date)->startOfDay());
+        $this->assertSame(Carbon::parse($period->start_date)->endOfMonth(), Carbon::parse($period->end_date));
+
+        // period has envelopes associated to it period->envelope
+        $this->assertDatabaseHas('envelopes', [
+            'budget_period_id' => $period->id
+        ]);
+        $this->assertDatabaseCount('envelopes', 3);
+    }
+
+
+    /**
+     * Tests with a previous end date.
+     */
+    public function testGeneratePeriodWithPreviousEndDate(): void
+    {
+        $this->assertTrue(true);
+    }
+
+
+    /**
+     * Tests when new period would exceed the plan's ends_at
+     */
+    public function testGeneratePeriodExceedingPlanEnd(): void
+    {
+        $this->assertTrue(true);
     }
 
     /**
@@ -21,7 +99,6 @@ class BudgetPlansTest extends TestCase
 
         $response->assertStatus(200);
     }
-
 
     /**
      * Test budget plan creation failed.
