280bd1fea9459056ac06b8c6fbe2f12e60d9ba07
Josh Sarnecki <joshsarnecki@gmail.com>
2026-04-25 21:07:04 -0700

    Get first test passing

--- backend/tests/Feature/BudgetPlansTest.php
+++ backend/tests/Feature/BudgetPlansTest.php
@@ -28,17 +28,6 @@ class BudgetPlansTest extends TestCase
      */
     public function testGeneratePeriodNoPreviousEndDate(): void
     {
-        /* $this->assertTrue(true); */
-
-        /* $plan = BudgetPlan::factory([ */
-        /*     'user_id'      => 1, */
-        /*     'name'         => 'test', */
-        /*     'period_type'  => 'monthly', */
-        /*     'total_amount' => 500, */
-        /*     'starts_at'    => Carbon::now()->format('y-m-d'), */
-        /*     'ends_at'      => null, */
-        /* ]); */
-
         $plan = BudgetPlan::factory()->create([
             'starts_at' => Carbon::now()->format('Y-m-d'),
             'ends_at'   => null,
@@ -49,28 +38,22 @@ class BudgetPlansTest extends TestCase
             ->for($plan)
             ->create();
 
-        // create PlanEnvelopes for BudgetPlan->associate()
-        /* $envelopes = BudgetPlanEnvelope::factory() */
-        /*     ->count(3) */
-        /*     ->for($plan) */
-        /*     ->create(); */
-
-        /* $envelopes->each(function ($envelope) use ($plan) { */
-        /*     $plan->planEnvelope->associate($envelope); */
-        /* }); */
-
         $period = $this->budgetPeriodService->generatePeriod($plan, null);
-        // assert period has plan_id, ..
-        $this->assertSame($plan->id, $period->budget_plan_id);
-        // period start date is same as plan (when null passed) and end date is end of the month
-        $this->assertSame(Carbon::parse($plan->starts_at)->startOfDay(), Carbon::parse($period->start_date)->startOfDay());
-        $this->assertSame(Carbon::parse($period->start_date)->endOfMonth(), Carbon::parse($period->end_date));
 
-        // period has envelopes associated to it period->envelope
+        // New period is tied to the BudgetPlan.
+        $this->assertSame($plan->id, $period->budget_plan_id);
+
+        // Period start date is same as BudgetPlan's (when null passed).
+        $this->assertTrue(Carbon::parse($plan->starts_at)->isSameDay(Carbon::parse($period->start_date)));
+
+        // Period's end date is end of the same month as it starts.
+        $this->assertTrue(Carbon::parse($period->start_date)->endOfMonth()->isSameDay(Carbon::parse($period->end_date)));
+
+        // Period had all 3 envelopes associated to it.
+        $this->assertDatabaseCount('envelopes', 3);
         $this->assertDatabaseHas('envelopes', [
             'budget_period_id' => $period->id
         ]);
-        $this->assertDatabaseCount('envelopes', 3);
     }
 
 
